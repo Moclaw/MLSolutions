df5dacd4a19a71a2949f0c9fb8f56aa9af282c4b
moclaw.nguyen <cong.nguyen@terralogic.com>
2025-05-22 11:36:38 +0700

    fix

--- src/Host/Services/CorsServices.cs
+++ src/Host/Services/CorsServices.cs
@@ -1,7 +1,7 @@
 ﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Shared.Constants;
+using Shared.Settings;
 
 namespace Host.Services;
 
